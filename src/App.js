6e65dcdec8b87ed767843f59b41d6f77216d2adf
Joey Dawe <dawekobe@gmail.com>
2023-05-23 08:40:20 +0800

    java

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navbarcom from 'src/Components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginPage from './pages/Login';
+import RegistrationPage from './pages/Registration';
+import {Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbarcom/>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+
+const  Home = () => {
+        return(
+            <div style={{ width: 600, margin: 'auto', marginTop: 40}}>
+                <Form/>
+            </div>
+        )
+
+
+};
+
+export default Home;
